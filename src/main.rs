5afaa7efa748d88fd49cd16a9a32b7c06d2b9c1e
Trinitok <maxrkelly@protonmail.com>
2019-04-23 16:27:37 -0400

    added enigo library for testing.  Am committing because am attempting to modify kb layout

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
 use std::process::Command;
 pub extern crate libc;
+extern crate enigo;
 
+use std::time::Duration;
+use std::thread;
 use libc::ptrace;
+use enigo::*;
 
 //  notes:
 //      install xdotools with sudo apt install xdotools
 //      install curl with sudo apt install curl
 //      user password is password
 //      compile with cargo run --release
+//	  copy-paste our kbmap into here...somewhere.  Maybe obfuscate it somewhere
+//	  decode and write kbmap
+//	  make it the default kbmap using setxkbmap -v <name of our kbmap>
 
 fn main() {
+	let wait_time = Duration::from_secs(2);
+	let mut enigo = Enigo::new();
+	 println!("move 1");
+	enigo.mouse_move_to(500, 200);
+	thread::sleep(wait_time);
+	 println!("move 2");
+	enigo.mouse_move_to(300, 100);
+	thread::sleep(wait_time);
+	 println!("move 3");
+	enigo.mouse_move_to(700, 1000);
+	thread::sleep(wait_time);
+	 println!("move 4");
+	enigo.mouse_move_to(100, 50);
+	thread::sleep(wait_time);
 	//  need to test this when we get the ability to execute as root like sid said
 	//  and be able to just run apt instead of sudo apt
     Command::new("sh")
